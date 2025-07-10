4d153cfab1d2251e339b31573aef00316c71bcc2
Aerin Gamaroff <aerin.gamaroff@gmail.com>
2025-07-10 10:58:46 +0200

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import reactLogo from "../assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <img src={reactLogo} />
+        <img src={viteLogo} />
+        <h1>Keeper</h1>
+      </header>
+    </>
+  );
+}
+
+export default Header;
